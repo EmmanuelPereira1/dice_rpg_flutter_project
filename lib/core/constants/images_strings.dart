58885d58efa215be96433582f9fde3336459d09e
EmmanuelPereira1 <97740932+EmmanuelPereira1@users.noreply.github.com>
2022-06-23 11:50:40 -0300

    wall_image_change

--- lib/core/constants/images_strings.dart
+++ lib/core/constants/images_strings.dart
@@ -5,6 +5,7 @@ class ImagesStrings {
   static String dice8 = 'assets/images/dice10.png';
   static String dice6 = 'assets/images/dice6.png';
   static String dice4 = 'assets/images/dice4.png';
+  static String background = "assets/images/wall.png";
   static String lottieSplashScrean =
       'https://assets8.lottiefiles.com/packages/lf20_hkgn7rqg.json';
 }
